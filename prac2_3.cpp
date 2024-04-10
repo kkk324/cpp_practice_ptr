db3a81d782b6e266fbdc39ae96ad425d0cee7e58
CRK <fightclubex324.com>
2024-04-10 19:07:43 +0800

    Refresh!

--- prac2_3.cpp
+++ prac2_3.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void swap(int a, int b){
-	int *tmp = &b;
+void swap(int *a, int *b){
+	int *tmp = b;
 	b = a;
-	a = *tmp;
+	a = tmp;
 }
 
 int main() {
 	int a = 10;
 	int b = 20;
 	cout << "a=" << a << ",b=" << b << endl;
-	swap(a,b);
+	swap(&a,&b);
 	cout << "a=" << a << ",b=" << b << endl; 
 }
